5fedbf8bd78b148ff367601e16a5ca9ef26b0ae9
Clément <clef-men@orange.fr>
2024-02-22 22:12:24 +0100

    Add boxroot/boxroot.

--- theories/boxroot/gc.v
+++ theories/boxroot/gc.v
@@ -19,6 +19,9 @@ Parameter gc_loc : Type.
 Implicit Types ω : gc_loc.
 Implicit Types ωs : list gc_loc.
 
+Parameter gc_loc_inhabited : Inhabited gc_loc.
+#[global] Existing Instance gc_loc_inhabited.
+
 Parameter gc_val : Type.
 Parameter GcInt : Z → gc_val.
 Parameter GcLoc : gc_loc → gc_val.
@@ -30,12 +33,10 @@ Implicit Types νs : list gc_val.
 Parameter gc_model : ∀ `{zebre_G : !ZebreG Σ}, gc_state → iProp Σ.
 Parameter gc_pointsto : ∀ `{zebre_G : !ZebreG Σ}, gc_loc → list gc_val → iProp Σ.
 Parameter gc_realized : gc_state → gc_loc → loc → Prop.
-Parameter gc_root : ∀ `{zebre_G : !ZebreG Σ}, loc → gc_loc → iProp Σ.
-Definition gc_maybe_root `{zebre_G : !ZebreG Σ} gc root ω : iProp Σ :=
-    gc_root root ω
-  ∨ ∃ l,
-    root ↦ #l ∗
-    ⌜gc_realized gc ω l⌝.
+Definition gc_root `{zebre_G : !ZebreG Σ} gc root ω : iProp Σ :=
+  ∃ l,
+  root ↦ #l ∗
+  ⌜gc_realized gc ω l⌝.
 
 Notation "ω '↦gc' νs" := (
   gc_pointsto ω νs
@@ -47,30 +48,16 @@ Notation "ω '↦gc[' gc ] l" := (
 )(at level 20,
   format "ω  ↦gc[ gc ]  l"
 ) : stdpp_scope.
-Notation "root '↦root' ω" := (
-  gc_root root ω
-)(at level 20,
-  format "root  ↦root  ω"
-) : bi_scope.
 Notation "root '↦root[' gc ] ω" := (
-  gc_maybe_root gc root ω
+  gc_root gc root ω
 )(at level 20,
   format "root  ↦root[ gc ]  ω"
 ) : bi_scope.
 
-Parameter gc_alloc : val.
-Axiom gc_alloc_spec : ∀ `{zebre_G : !ZebreG Σ} gc n,
-  (0 < n)%Z →
-  {{{
-    gc_model gc
-  }}}
-    gc_alloc #n
-  {{{ l gc ω,
-    RET #l;
-    ⌜ω ↦gc[gc] l⌝ ∗
-    gc_model gc ∗
-    ω ↦gc replicate (Z.to_nat n) (GcInt 0)
-  }}}.
+Axiom gc_realized_agree : ∀ gc ω l1 l2,
+  ω ↦gc[gc] l1 →
+  ω ↦gc[gc] l2 →
+  l1 = l2.
 
 Parameter wp_load_gc : ∀ `{zebre_G : !ZebreG Σ} ν gc ω νs l i,
   (0 ≤ i)%Z →
@@ -102,49 +89,108 @@ Parameter wp_store_gc : ∀ `{zebre_G : !ZebreG Σ} ν gc ω νs l i v,
     ω ↦gc <[Z.to_nat i := ν]> νs
   }}}.
 
-Axiom wp_load_gc_root : ∀ `{zebre_G : !ZebreG Σ} gc root ω,
+Lemma wp_load_gc_root `{zebre_G : !ZebreG Σ} gc root ω :
   {{{
-    gc_model gc ∗
-    root ↦root ω
+    root ↦root[gc] ω
   }}}
     !#root
   {{{ l,
     RET #l;
     ⌜ω ↦gc[gc] l⌝ ∗
-    gc_model gc ∗
-    root ↦root ω
+    root ↦root[gc] ω
   }}}.
-
-Parameter gc_roots : ∀ `{zebre_G : !ZebreG Σ}, val → (list loc → iProp Σ) → iProp Σ.
-Parameter gc_set_roots : val.
-Axiom gc_set_roots_spec : ∀ `{zebre_G : !ZebreG Σ} iter (Χ : list loc → iProp Σ),
+Proof.
+  iSteps.
+Qed.
+Lemma wp_load_gc_root' `{zebre_G : !ZebreG Σ} {gc root ω} l :
+  ω ↦gc[gc] l →
   {{{
-    ∀ Ψ gc roots ωs fn,
-    {{{
-      ▷ Ψ [] ∗
-      Χ roots ∗
-      ( [∗ list] root; ω ∈ roots; ωs,
-        root ↦root[gc] ω
-      ) ∗
-      □ (
-        ∀ roots_done root roots_todo,
-        ⌜roots = roots_done ++ root :: roots_todo⌝ -∗
-        Ψ roots_done -∗
-        WP fn #root {{ res,
-          ⌜res = ()%V⌝ ∗
-          ▷ Ψ (roots_done ++ [root])
-        }}
-      )
-    }}}
-      iter fn
-    {{{
-      RET ();
-      Χ roots ∗
-      Ψ roots
-    }}}
+    root ↦root[gc] ω
+  }}}
+    !#root
+  {{{
+    RET #l;
+    root ↦root[gc] ω
+  }}}.
+Proof.
+  iIntros "%Hω %Φ (%_l & Hroot & %_Hω) HΦ".
+  opose proof* (gc_realized_agree _ _ l _l) as <-; [done.. |].
+  iSteps.
+Qed.
+
+Lemma wp_store_gc_root `{zebre_G : !ZebreG Σ} {gc root ω'} ω l :
+  ω ↦gc[gc] l →
+  {{{
+    root ↦root[gc] ω'
+  }}}
+    #root <- #l
+  {{{
+    RET ();
+    root ↦root[gc] ω
+  }}}.
+Proof.
+  iSteps.
+Qed.
+
+Parameter gc_roots : ∀ `{zebre_G : !ZebreG Σ}, (gc_state → iProp Σ) → iProp Σ.
+Parameter gc_set_roots : val.
+Axiom gc_set_roots_spec : ∀ `{zebre_G : !ZebreG Σ} {gc Χ' iter} Χ Ξ,
+  {{{
+    gc_model gc ∗
+    gc_roots Χ' ∗
+    □ (
+      ∀ gc,
+      Χ gc ∗-∗
+        ∃ roots ωs,
+        Ξ roots ωs ∗
+        ( [∗ list] root; ω ∈ roots; ωs,
+          root ↦root[gc] ω
+        )
+    ) ∗
+    □ (
+      ∀ Ψ roots ωs fn,
+      {{{
+        ▷ Ψ [] ∗
+        Ξ roots ωs ∗
+        □ (
+          ∀ roots_done root roots_todo,
+          ⌜roots = roots_done ++ root :: roots_todo⌝ -∗
+          Ψ roots_done -∗
+          WP fn #root {{ res,
+            ⌜res = ()%V⌝ ∗
+            ▷ Ψ (roots_done ++ [root])
+          }}
+        )
+      }}}
+        iter fn
+      {{{
+        RET ();
+        Ξ roots ωs ∗
+        Ψ roots
+      }}}
+    )
   }}}
     gc_set_roots iter
   {{{
     RET ();
-    gc_roots iter Χ
+    gc_model gc ∗
+    gc_roots Χ
+  }}}.
+
+Parameter gc_alloc : val.
+Axiom gc_alloc_spec : ∀ `{zebre_G : !ZebreG Σ} gc Χ n,
+  (0 < n)%Z →
+  {{{
+    gc_model gc ∗
+    gc_roots Χ ∗
+    Χ gc
+  }}}
+    gc_alloc #n
+  {{{ l gc ω,
+    RET #l;
+    ⌜ω ↦gc[gc] l⌝ ∗
+    gc_model gc ∗
+    gc_roots Χ ∗
+    Χ gc ∗
+    ω ↦gc replicate (Z.to_nat n) (GcInt 0)
   }}}.
---
+++ theories/std/xdeque.v
@@ -0,0 +1,78 @@
+From zebre Require Import
+  prelude.
+From zebre.language Require Import
+  notations.
+From zebre.std Require Export
+  base.
+From zebre Require Import
+  options.
+
+Implicit Types l slot : loc.
+Implicit Types slots : list loc.
+Implicit Types t fn : val.
+
+Parameter xdeque_create : val.
+
+Parameter xdeque_push_back : val.
+
+Parameter xdeque_remove : val.
+
+Parameter xdeque_iter : val.
+
+Parameter xdeque_model : ∀ `{zebre_G : !ZebreG Σ}, val → list loc → iProp Σ.
+
+Axiom xdeque_model_no_dup : ∀ `{zebre_G : !ZebreG Σ} t slots,
+  xdeque_model t slots ⊢
+  ⌜NoDup slots⌝.
+
+Axiom xdeque_create_spec : ∀ `{zebre_G : !ZebreG Σ},
+  {{{ True }}}
+    xdeque_create ()
+  {{{ t,
+    RET t;
+    (∃ l, ⌜t = #l⌝ ∗ meta_token l ⊤) ∗
+    xdeque_model t []
+  }}}.
+
+Axiom xdeque_push_back_spec : ∀ `{zebre_G : !ZebreG Σ} t slots v,
+  {{{
+    xdeque_model t slots
+  }}}
+    xdeque_push_back t v
+  {{{ slot,
+    RET #slot;
+    xdeque_model t (slots ++ [slot]) ∗
+    slot ↦ v
+  }}}.
+
+Axiom xdeque_remove_spec : ∀ `{zebre_G : !ZebreG Σ} {t slots slot} i,
+  slots !! i = Some slot →
+  {{{
+    xdeque_model t slots
+  }}}
+    xdeque_remove t #slot
+  {{{
+    RET ();
+    xdeque_model t (delete i slots)
+  }}}.
+
+Axiom xdeque_iter_spec : ∀ `{zebre_G : !ZebreG Σ} Ψ t slots fn,
+  {{{
+    ▷ Ψ [] ∗
+    xdeque_model t slots ∗
+    □ (
+      ∀ slots_done slot slots_todo,
+      ⌜slots = slots_done ++ slot :: slots_todo⌝ -∗
+      Ψ slots_done -∗
+      WP fn #slot {{ res,
+        ⌜res = ()%V⌝ ∗
+        ▷ Ψ (slots_done ++ [#slot])
+      }}
+    )
+  }}}
+    xdeque_iter t fn
+  {{{
+    RET ();
+    xdeque_model t slots ∗
+    Ψ slots
+  }}}.
