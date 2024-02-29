c9f6dd516cb1973a28387a6e90e44f8edcc6bac5
Alexandre Moine <alexandre@moine.me>
2024-02-29 10:45:41 +0100

    ...

--- theories/store.v
+++ theories/store.v
@@ -1,7 +1,3 @@
-(* Based on:
-   https://gitlab.com/basile.clement/store/-/blob/main/src/store.ml?ref_type=heads
-*)
-
 From zebre Require Import
   prelude.
 From zebre.iris.base_logic Require Import
