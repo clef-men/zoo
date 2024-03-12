ade9aeb701533b4a39ad03760d55bc8de48ff3af
Clément <clef-men@orange.fr>
2024-03-12 20:58:06 +0100

    saturn/treiber_stack → saturn/mpmc_stack.

--- theories/saturn/mpmc_stack.v
+++ theories/saturn/mpmc_stack.v
@@ -24,20 +24,20 @@ Implicit Types l : location.
 Implicit Types v t : val.
 Implicit Types vs : list val.
 
-Definition treiber_stack_create : val :=
+Definition mpmc_stack_create : val :=
   λ: <>,
     ref §Nil.
 
-Definition treiber_stack_push : val :=
-  rec: "treiber_stack_push" "t" "v" :=
+Definition mpmc_stack_push : val :=
+  rec: "mpmc_stack_push" "t" "v" :=
     let: "old" := !"t" in
     let: "new" := ‘Cons{ "v", "old" } in
     ifnot: Cas "t" "old" "new" then (
-      "treiber_stack_push" "t" "v"
+      "mpmc_stack_push" "t" "v"
     ).
 
-Definition treiber_stack_pop : val :=
-  rec: "treiber_stack_pop" "t" :=
+Definition mpmc_stack_pop : val :=
+  rec: "mpmc_stack_pop" "t" :=
     let: "old" := !"t" in
     match: "old" with
     | Nil =>
@@ -46,86 +46,86 @@ Definition treiber_stack_pop : val :=
         if: Cas "t" "old" "new" then (
           ‘Some{ "v" }
         ) else (
-          "treiber_stack_pop" "t"
+          "mpmc_stack_pop" "t"
         )
     end.
 
-Class TreiberStackG Σ `{zebre_G : !ZebreG Σ} := {
-  #[local] treiber_stack_G_model_G :: TwinsG Σ (listO val) ;
+Class MpmcStackG Σ `{zebre_G : !ZebreG Σ} := {
+  #[local] mpmc_stack_G_model_G :: TwinsG Σ (listO val) ;
 }.
 
-Definition treiber_stack_Σ := #[
+Definition mpmc_stack_Σ := #[
   twins_Σ (listO val)
 ].
-#[global] Instance subG_treiber_stack_Σ Σ `{zebre_G : !ZebreG Σ} :
-  subG treiber_stack_Σ Σ →
-  TreiberStackG Σ.
+#[global] Instance subG_mpmc_stack_Σ Σ `{zebre_G : !ZebreG Σ} :
+  subG mpmc_stack_Σ Σ →
+  MpmcStackG Σ.
 Proof.
   solve_inG.
 Qed.
 
 Section zebre_G.
-  Context `{treiber_stack_G : TreiberStackG Σ}.
+  Context `{mpmc_stack_G : MpmcStackG Σ}.
 
-  #[local] Definition treiber_stack_model₁ γ vs :=
+  #[local] Definition mpmc_stack_model₁ γ vs :=
     twins_twin1 γ (DfracOwn 1) vs.
-  #[local] Definition treiber_stack_model₂ γ vs :=
+  #[local] Definition mpmc_stack_model₂ γ vs :=
     twins_twin2 γ vs.
 
-  #[local] Definition treiber_stack_inv_inner l γ : iProp Σ :=
+  #[local] Definition mpmc_stack_inv_inner l γ : iProp Σ :=
     ∃ vs,
     l ↦ (lst_to_val vs) ∗
-    treiber_stack_model₂ γ vs.
-  Definition treiber_stack_inv t ι : iProp Σ :=
+    mpmc_stack_model₂ γ vs.
+  Definition mpmc_stack_inv t ι : iProp Σ :=
     ∃ l γ,
     ⌜t = #l⌝ ∗
     meta l nroot γ ∗
-    inv ι (treiber_stack_inv_inner l γ).
+    inv ι (mpmc_stack_inv_inner l γ).
 
-  Definition treiber_stack_model t vs : iProp Σ :=
+  Definition mpmc_stack_model t vs : iProp Σ :=
     ∃ l γ,
     ⌜t = #l⌝ ∗
     meta l nroot γ ∗
-    treiber_stack_model₁ γ vs.
+    mpmc_stack_model₁ γ vs.
 
-  #[global] Instance treiber_stack_model_timeless t vs :
-    Timeless (treiber_stack_model t vs).
+  #[global] Instance mpmc_stack_model_timeless t vs :
+    Timeless (mpmc_stack_model t vs).
   Proof.
     apply _.
   Qed.
-  #[global] Instance treiber_stack_inv_persistent t ι :
-    Persistent (treiber_stack_inv t ι).
+  #[global] Instance mpmc_stack_inv_persistent t ι :
+    Persistent (mpmc_stack_inv t ι).
   Proof.
     apply _.
   Qed.
 
-  Lemma treiber_stack_create_spec ι :
+  Lemma mpmc_stack_create_spec ι :
     {{{ True }}}
-      treiber_stack_create ()
+      mpmc_stack_create ()
     {{{ t,
       RET t;
-      treiber_stack_inv t ι ∗
-      treiber_stack_model t []
+      mpmc_stack_inv t ι ∗
+      mpmc_stack_model t []
     }}}.
   Proof.
     iIntros "%Φ _ HΦ".
     wp_rec.
     wp_alloc l as "Hmeta" "Hl".
-    iMod (twins_alloc' (twins_G := treiber_stack_G_model_G) []) as "(%γ & Hmodel₁ & Hmodel₂)".
+    iMod (twins_alloc' (twins_G := mpmc_stack_G_model_G) []) as "(%γ & Hmodel₁ & Hmodel₂)".
     iMod (meta_set with "Hmeta") as "#Hmeta"; first done.
     iApply "HΦ". iSplitR "Hmodel₁"; last iSteps.
     iStep 2. iApply inv_alloc. iExists []. iSteps.
   Qed.
 
-  Lemma treiber_stack_push_spec t ι v :
+  Lemma mpmc_stack_push_spec t ι v :
     <<<
-      treiber_stack_inv t ι
+      mpmc_stack_inv t ι
     | ∀∀ vs,
-      treiber_stack_model t vs
+      mpmc_stack_model t vs
     >>>
-      treiber_stack_push t v @ ↑ι
+      mpmc_stack_push t v @ ↑ι
     <<<
-      treiber_stack_model t (v :: vs)
+      mpmc_stack_model t (v :: vs)
     | RET (); True
     >>>.
   Proof.
@@ -148,21 +148,21 @@ Section zebre_G.
     iMod "HΦ" as "(%_vs & (%_l & %_γ & %Heq & _Hmeta & Hmodel₁) & _ & HΦ)". injection Heq as <-.
     iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
     iDestruct (twins_agree_L with "Hmodel₁ Hmodel₂") as %->.
-    iMod (twins_update' (twins_G := treiber_stack_G_model_G) (v :: vs) with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
+    iMod (twins_update' (twins_G := mpmc_stack_G_model_G) (v :: vs) with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
     iMod ("HΦ" with "[Hmodel₁]") as "HΦ"; first iSteps.
     iSplitR "HΦ". { iExists (v :: vs). iSteps. }
     iSteps.
   Qed.
 
-  Lemma treiber_stack_pop_spec t ι :
+  Lemma mpmc_stack_pop_spec t ι :
     <<<
-      treiber_stack_inv t ι
+      mpmc_stack_inv t ι
     | ∀∀ vs,
-      treiber_stack_model t vs
+      mpmc_stack_model t vs
     >>>
-      treiber_stack_pop t @ ↑ι
+      mpmc_stack_pop t @ ↑ι
     <<<
-      treiber_stack_model t (tail vs)
+      mpmc_stack_model t (tail vs)
     | RET head vs; True
     >>>.
   Proof.
@@ -195,16 +195,16 @@ Section zebre_G.
       iMod "HΦ" as "(%_vs & (%_l & %_γ & %Heq & _Hmeta & Hmodel₁) & _ & HΦ)". injection Heq as <-.
       iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
       iDestruct (twins_agree_L with "Hmodel₁ Hmodel₂") as %->.
-      iMod (twins_update' (twins_G := treiber_stack_G_model_G) vs with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
+      iMod (twins_update' (twins_G := mpmc_stack_G_model_G) vs with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
       iMod ("HΦ" with "[Hmodel₁]") as "HΦ"; first iSteps.
       iSplitR "HΦ"; first iSteps.
       iSteps.
   Qed.
 End zebre_G.
 
-#[global] Opaque treiber_stack_create.
-#[global] Opaque treiber_stack_push.
-#[global] Opaque treiber_stack_pop.
+#[global] Opaque mpmc_stack_create.
+#[global] Opaque mpmc_stack_push.
+#[global] Opaque mpmc_stack_pop.
 
-#[global] Opaque treiber_stack_inv.
-#[global] Opaque treiber_stack_model.
+#[global] Opaque mpmc_stack_inv.
+#[global] Opaque mpmc_stack_model.
