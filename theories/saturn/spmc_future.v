bc41e56f4ab510f5f4cca01df7516437cc07a98b
Clément <clef-men@orange.fr>
2024-03-19 18:40:02 +0100

    Add saturn/spsc_future and saturn/spmc_future (not verified yet).

---
+++ theories/saturn/spmc_future.v
@@ -0,0 +1,56 @@
+From zebre Require Import
+  prelude.
+From zebre.language Require Import
+  notations.
+From zebre.std Require Import
+  opt
+  condition.
+From zebre.saturn Require Export
+  base.
+From zebre Require Import
+  options.
+
+#[local] Notation "'result'" := (
+  in_type "t" 0
+)(in custom zebre_field
+).
+#[local] Notation "'mutex'" := (
+  in_type "t" 1
+)(in custom zebre_field
+).
+#[local] Notation "'condition'" := (
+  in_type "t" 2
+)(in custom zebre_field
+).
+
+Definition spmc_future_create : val :=
+  λ: <>,
+    { §None;
+      mutex_create ();
+      condition_create ()
+    }.
+
+Definition spmc_future_set : val :=
+  λ: "t" "v",
+    "t" <-{result} ‘Some{ "v" }.
+
+Definition spmc_future_try_get : val :=
+  λ: "t",
+    "t".{result}.
+Definition spmc_future_get : val :=
+  λ: "t",
+    match: spmc_future_try_get "t" with
+    | Some "v" =>
+        "v"
+    | None =>
+        let: "mtx" := "t".{mutex} in
+        let: "cond" := "t".{condition} in
+        mutex_protect "mtx" (λ: <>,
+          condition_wait_while "cond" "mtx" (λ: <>, "t".{result} = §None)
+        )
+    end.
+
+#[global] Opaque spmc_future_create.
+#[global] Opaque spmc_future_set.
+#[global] Opaque spmc_future_try_get.
+#[global] Opaque spmc_future_get.
---
+++ theories/saturn/spsc_future.v
@@ -0,0 +1,56 @@
+From zebre Require Import
+  prelude.
+From zebre.language Require Import
+  notations.
+From zebre.std Require Import
+  opt
+  condition.
+From zebre.saturn Require Export
+  base.
+From zebre Require Import
+  options.
+
+#[local] Notation "'result'" := (
+  in_type "t" 0
+)(in custom zebre_field
+).
+#[local] Notation "'mutex'" := (
+  in_type "t" 1
+)(in custom zebre_field
+).
+#[local] Notation "'condition'" := (
+  in_type "t" 2
+)(in custom zebre_field
+).
+
+Definition spsc_future_create : val :=
+  λ: <>,
+    { §None;
+      mutex_create ();
+      condition_create ()
+    }.
+
+Definition spsc_future_set : val :=
+  λ: "t" "v",
+    "t" <-{result} ‘Some{ "v" }.
+
+Definition spsc_future_try_get : val :=
+  λ: "t",
+    "t".{result}.
+Definition spsc_future_get : val :=
+  λ: "t",
+    match: spsc_future_try_get "t" with
+    | Some "v" =>
+        "v"
+    | None =>
+        let: "mtx" := "t".{mutex} in
+        let: "cond" := "t".{condition} in
+        mutex_protect "mtx" (λ: <>,
+          condition_wait_while "cond" "mtx" (λ: <>, "t".{result} = §None)
+        )
+    end.
+
+#[global] Opaque spsc_future_create.
+#[global] Opaque spsc_future_set.
+#[global] Opaque spsc_future_try_get.
+#[global] Opaque spsc_future_get.
