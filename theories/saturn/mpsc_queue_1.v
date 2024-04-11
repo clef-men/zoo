0506aa4d82605d1499a9896a6310a676a26a2fac
Clément <clef-men@orange.fr>
2024-04-12 00:18:03 +0200

    Add another saturn/mpsc_queue based on Michael-Scott.

--- theories/saturn/mpsc_queue_1.v
+++ theories/saturn/mpsc_queue_1.v
@@ -1,31 +1,18 @@
-(* Based on:
-   https://github.com/ocaml-multicore/picos/blob/07d6c2d391e076b490098c0379d01208b3a9cc96/test/lib/foundation/mpsc_queue.ml
-*)
-
-From iris.algebra Require Import
-  list.
-
 From zebre Require Import
   prelude.
-From zebre.common Require Import
-  list.
-From zebre.iris.base_logic Require Import
-  lib.twins.
 From zebre.language Require Import
   notations
   diaframe.
 From zebre.std Require Import
   opt
-  lst.
+  node2_chain.
 From zebre.saturn Require Export
   base.
 From zebre Require Import
   options.
 
-Implicit Types l : location.
 Implicit Types v t : val.
-Implicit Types vs front back : list val.
-Implicit Types o : option val.
+Implicit Types vs : list val.
 
 #[local] Notation "'front'" := (
   in_type "t" 0
@@ -38,185 +25,83 @@ Implicit Types o : option val.
 
 Definition mpsc_queue_create : val :=
   λ: <>,
-    { §Nil; §Nil }.
+    let: "front" := node2_create () () in
+    { "front"; "front" }.
 
-Definition mpsc_queue_push : val :=
-  rec: "mpsc_queue_push" "t" "v" :=
+Definition mpsc_queue_is_empty : val :=
+  λ: "t",
+    "t".{front}.{node2_next} = ().
+
+#[local] Definition mpsc_queue_do_push : val :=
+  rec: "mpsc_queue_do_push" "node" "new_back" :=
+    let: "node'" := "node".{node2_next} in
+    ifnot: "node'" = () and Cas "node".[node2_next] () "new_back" then
+      "mpsc_queue_do_push" "node'" "new_back".
+#[local] Definition mpsc_queue_fix_back : val :=
+  rec: "mpsc_queue_fix_back" "t" "new_back" :=
     let: "back" := "t".{back} in
-    if: Cas "t".[back] "back" ‘Cons{ "v", "back" } then (
-      ()
-    ) else (
-      "mpsc_queue_push" "t" "v"
-    ).
+    if: "new_back".{node2_next} = () and ~ Cas "t".[back] "back" "new_back" then
+      "mpsc_queue_fix_back" "t" "new_back".
+Definition mpsc_queue_push : val :=
+  λ: "t" "v",
+    let: "new_back" := node2_create "v" () in
+    mpsc_queue_do_push "t".{back} "new_back" ;;
+    mpsc_queue_fix_back "t" "new_back".
 
 Definition mpsc_queue_pop : val :=
   λ: "t",
-    match: "t".{front} with
-    | Nil =>
-        match: lst_rev (Xchg "t".[back] §Nil) with
-        | Nil =>
-            §None
-        | Cons "v" "front" =>
-            "t" <-{front} "front" ;;
-            ‘Some{ "v" }
-        end
-    | Cons "v" "front" =>
-        "t" <-{front} "front" ;;
-        ‘Some{ "v" }
-    end.
+    let: "front" := "t".{front}.{node2_next} in
+    if: "front" = () then (
+      §None
+    ) else (
+      "t" <-{front} "front" ;;
+      let: "v" := "front".{node2_data} in
+      "front" <-{node2_data} () ;;
+      ‘Some{ "v" }
+    ).
 
 Class MpscQueueG Σ `{zebre_G : !ZebreG Σ} := {
-  #[local] mpsc_queue_G_twins_G :: TwinsG Σ (listO val) ;
 }.
 
 Definition mpsc_queue_Σ := #[
-  twins_Σ (listO val)
 ].
 #[global] Instance subG_mpsc_queue_Σ Σ `{zebre_G : !ZebreG Σ} :
   subG mpsc_queue_Σ Σ →
   MpscQueueG Σ.
 Proof.
-  solve_inG.
 Qed.
 
 Section mpsc_queue_G.
   Context `{mpsc_queue_G : MpscQueueG Σ}.
 
-  Record mpsc_queue_meta := {
-    mpsc_queue_meta_model : gname ;
-    mpsc_queue_meta_front : gname ;
-  }.
-  Implicit Types γ : mpsc_queue_meta.
+  Definition mpsc_queue_inv t (ι : namespace) : iProp Σ.
+  Proof. Admitted.
 
-  #[local] Instance mpsc_queue_meta_eq_dec : EqDecision mpsc_queue_meta :=
-    ltac:(solve_decision).
-  #[local] Instance mpsc_queue_meta_countable :
-    Countable mpsc_queue_meta.
-  Proof.
-    pose encode γ := (
-      γ.(mpsc_queue_meta_model),
-      γ.(mpsc_queue_meta_front)
-    ).
-    pose decode := λ '(γ_model, γ_front), {|
-      mpsc_queue_meta_model := γ_model ;
-      mpsc_queue_meta_front := γ_front ;
-    |}.
-    refine (inj_countable' encode decode _). intros []. done.
-  Qed.
+  Definition mpsc_queue_model t vs : iProp Σ.
+  Proof. Admitted.
 
-  #[local] Definition mpsc_queue_model₁' γ_model vs :=
-    twins_twin1 γ_model (DfracOwn 1) vs.
-  #[local] Definition mpsc_queue_model₁ γ vs :=
-    mpsc_queue_model₁' γ.(mpsc_queue_meta_model) vs.
-  #[local] Definition mpsc_queue_model₂' γ_model vs :=
-    twins_twin2 γ_model vs.
-  #[local] Definition mpsc_queue_model₂ γ vs :=
-    mpsc_queue_model₂' γ.(mpsc_queue_meta_model) vs.
-
-  #[local] Definition mpsc_queue_front₁' γ_front front :=
-    twins_twin1 γ_front (DfracOwn 1) front.
-  #[local] Definition mpsc_queue_front₁ γ front :=
-    mpsc_queue_front₁' γ.(mpsc_queue_meta_front) front.
-  #[local] Definition mpsc_queue_front₂' γ_model front :=
-    twins_twin2 γ_model front.
-  #[local] Definition mpsc_queue_front₂ γ front :=
-    mpsc_queue_front₂' γ.(mpsc_queue_meta_front) front.
-
-  #[local] Definition mpsc_queue_inv_inner l γ : iProp Σ :=
-    ∃ front back,
-    mpsc_queue_front₂ γ front ∗
-    l.[back] ↦ lst_to_val back ∗
-    mpsc_queue_model₂ γ (front ++ reverse back).
-  Definition mpsc_queue_inv t ι : iProp Σ :=
-    ∃ l γ,
-    ⌜t = #l⌝ ∗
-    meta l nroot γ ∗
-    inv ι (mpsc_queue_inv_inner l γ).
-
-  Definition mpsc_queue_model t vs : iProp Σ :=
-    ∃ l γ,
-    ⌜t = #l⌝ ∗
-    meta l nroot γ ∗
-    mpsc_queue_model₁ γ vs.
-
-  Definition mpsc_queue_consumer t : iProp Σ :=
-    ∃ l γ front,
-    ⌜t = #l⌝ ∗
-    meta l nroot γ ∗
-    l.[front] ↦ lst_to_val front ∗
-    mpsc_queue_front₁ γ front.
+  Definition mpsc_queue_consumer t : iProp Σ.
+  Proof. Admitted.
 
   #[global] Instance mpsc_queue_model_timeless t vs :
     Timeless (mpsc_queue_model t vs).
   Proof.
-    apply _.
-  Qed.
+  Admitted.
   #[global] Instance mpsc_queue_consumer_timeless t :
     Timeless (mpsc_queue_consumer t ).
   Proof.
-    apply _.
-  Qed.
+  Admitted.
   #[global] Instance mpsc_queue_inv_persistent t ι :
     Persistent (mpsc_queue_inv t ι).
   Proof.
-    apply _.
-  Qed.
-
-  #[local] Lemma mpsc_queue_model_alloc :
-    ⊢ |==>
-      ∃ γ_model,
-      mpsc_queue_model₁' γ_model [] ∗
-      mpsc_queue_model₂' γ_model [].
-  Proof.
-    apply twins_alloc'.
-  Qed.
-  #[local] Lemma mpsc_queue_model_agree γ vs1 vs2 :
-    mpsc_queue_model₁ γ vs1 -∗
-    mpsc_queue_model₂ γ vs2 -∗
-    ⌜vs1 = vs2⌝.
-  Proof.
-    apply: twins_agree_L.
-  Qed.
-  #[local] Lemma mpsc_queue_model_update {γ vs1 vs2} vs :
-    mpsc_queue_model₁ γ vs1 -∗
-    mpsc_queue_model₂ γ vs2 ==∗
-      mpsc_queue_model₁ γ vs ∗
-      mpsc_queue_model₂ γ vs.
-  Proof.
-    apply twins_update'.
-  Qed.
-
-  #[local] Lemma mpsc_queue_front_alloc :
-    ⊢ |==>
-      ∃ γ_front,
-      mpsc_queue_front₁' γ_front [] ∗
-      mpsc_queue_front₂' γ_front [].
-  Proof.
-    apply twins_alloc'.
-  Qed.
-  #[local] Lemma mpsc_queue_front_agree γ front1 front2 :
-    mpsc_queue_front₁ γ front1 -∗
-    mpsc_queue_front₂ γ front2 -∗
-    ⌜front1 = front2⌝.
-  Proof.
-    apply: twins_agree_L.
-  Qed.
-  #[local] Lemma mpsc_queue_front_update {γ front1 front2} front :
-    mpsc_queue_front₁ γ front1 -∗
-    mpsc_queue_front₂ γ front2 ==∗
-      mpsc_queue_front₁ γ front ∗
-      mpsc_queue_front₂ γ front.
-  Proof.
-    apply twins_update'.
-  Qed.
+  Admitted.
 
   Lemma mpsc_queue_consumer_exclusive t :
     mpsc_queue_consumer t -∗
     mpsc_queue_consumer t -∗
     False.
   Proof.
-    iSteps.
-  Qed.
+  Admitted.
 
   Lemma mpsc_queue_create_spec ι :
     {{{ True }}}
@@ -228,26 +113,7 @@ Section mpsc_queue_G.
       mpsc_queue_consumer t
     }}}.
   Proof.
-    iIntros "%Φ _ HΦ".
-
-    wp_rec.
-
-    wp_record l as "Hmeta" "(Hfront & Hback & _)".
-
-    iMod mpsc_queue_model_alloc as "(%γ_model & Hmodel₁ & Hmodel₂)".
-    iMod mpsc_queue_front_alloc as "(%γ_front & Hfront₁ & Hfront₂)".
-
-    pose γ := {|
-      mpsc_queue_meta_model := γ_model ;
-      mpsc_queue_meta_front := γ_front ;
-    |}.
-    iMod (meta_set _ _ γ with "Hmeta") as "#Hmeta"; first done.
-
-    iApply "HΦ".
-    iSplitR "Hmodel₁ Hfront Hfront₁".
-    - iExists l, γ. iStep 2. iApply inv_alloc. iExists [], []. iSteps.
-    - iSplitL "Hmodel₁"; first iSteps. iExists l, γ, []. iSteps.
-  Qed.
+  Admitted.
 
   Lemma mpsc_queue_push_spec t ι v :
     <<<
@@ -261,32 +127,7 @@ Section mpsc_queue_G.
     | RET (); True
     >>>.
   Proof.
-    iIntros "!> %Φ (%l & %γ & -> & #Hmeta & #Hinv) HΦ".
-
-    iLöb as "HLöb".
-
-    wp_rec. wp_pures.
-
-    wp_bind (!_)%E.
-    iInv "Hinv" as "(%front & %back & Hfront₂ & Hback & Hmodel₂)".
-    wp_load.
-    iSplitR "HΦ"; first iSteps.
-    iModIntro. clear.
-
-    wp_pures.
-
-    wp_bind (Cas _ _ _).
-    iInv "Hinv" as "(%front & %back' & Hfront₂ & Hback & Hmodel₂)".
-    wp_cas as _ | ->%(inj _) _; first iSteps.
-    iMod "HΦ" as "(%vs & (%_l & %_γ & %Heq & _Hmeta & Hmodel₁) & _ & HΦ)". injection Heq as <-.
-    iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
-    iDestruct (mpsc_queue_model_agree with "Hmodel₁ Hmodel₂") as %Hvs.
-    iMod (mpsc_queue_model_update (vs ++ [v]) with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
-    iMod ("HΦ" with "[Hmodel₁]") as "HΦ"; first iSteps.
-    iSplitR "HΦ".
-    { iSteps. iExists (v :: back). iSteps. rewrite reverse_cons -assoc //. }
-    iSteps.
-  Qed.
+  Admitted.
 
   Lemma mpsc_queue_pop_spec t ι :
     <<<
@@ -302,58 +143,11 @@ Section mpsc_queue_G.
       mpsc_queue_consumer t
     >>>.
   Proof.
-    iIntros "!> %Φ ((%l & %γ & -> & #Hmeta & #Hinv) & (%_l & %_γ & %front & %Heq & _Hmeta & Hfront & Hfront₁)) HΦ". injection Heq as <-.
-    iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
-
-    wp_rec. wp_load.
-    destruct front as [| v front]; wp_pures.
-
-    - wp_bind (Xchg _ _).
-      iInv "Hinv" as "(%_front & %back & Hfront₂ & Hback & Hmodel₂)".
-      wp_xchg.
-      iDestruct (mpsc_queue_front_agree with "Hfront₁ Hfront₂") as %<-.
-      iMod "HΦ" as "(%vs & (%_l & %_γ & %Heq & _Hmeta & Hmodel₁) & _ & HΦ)". injection Heq as <-.
-      iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
-      iDestruct (mpsc_queue_model_agree with "Hmodel₁ Hmodel₂") as %->.
-      destruct (rev_elim back) as [-> | (back' & v & ->)].
-
-      + iMod ("HΦ" with "[Hmodel₁]") as "HΦ"; first iSteps.
-        iSplitR "Hfront Hfront₁ HΦ". { iExists [], []. iSteps. }
-        iModIntro. clear.
-
-        wp_apply (lst_rev_spec _ [] with "[//]") as "%back ->".
-        wp_pures.
-
-        iApply "HΦ".
-        iExists l, γ, []. iSteps.
-
-      + set front := reverse back'.
-        iMod (mpsc_queue_front_update front with "Hfront₁ Hfront₂") as "(Hfront₁ & Hfront₂)".
-        iMod (mpsc_queue_model_update front with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
-        iMod ("HΦ" with "[Hmodel₁]") as "HΦ".
-        { rewrite reverse_snoc. iSteps. }
-        iSplitR "Hfront Hfront₁ HΦ".
-        { iExists front, []. iSteps. rewrite right_id //. }
-        iModIntro. clear.
-
-        wp_apply (lst_rev_spec with "[//]") as "%back ->". rewrite reverse_snoc.
-        iSteps.
-
-    - wp_store.
-      iApply fupd_wp. iInv "Hinv" as "(%_front & %back & >Hfront₂ & Hback & >Hmodel₂)".
-      iDestruct (mpsc_queue_front_agree with "Hfront₁ Hfront₂") as %<-.
-      iMod (mpsc_queue_front_update with "Hfront₁ Hfront₂") as "(Hfront₁ & Hfront₂)".
-      iMod "HΦ" as "(%vs & (%_l & %_γ & %Heq & _Hmeta & Hmodel₁) & _ & HΦ)". injection Heq as <-.
-      iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
-      iDestruct (mpsc_queue_model_agree with "Hmodel₁ Hmodel₂") as %Hvs.
-      set vs' := front ++ reverse back.
-      iMod (mpsc_queue_model_update vs' with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
-      iMod ("HΦ" with "[Hmodel₁]") as "HΦ"; first iSteps.
-      iSteps.
-  Qed.
+  Admitted.
 End mpsc_queue_G.
 
 #[global] Opaque mpsc_queue_create.
+#[global] Opaque mpsc_queue_is_empty.
 #[global] Opaque mpsc_queue_push.
 #[global] Opaque mpsc_queue_pop.
 
