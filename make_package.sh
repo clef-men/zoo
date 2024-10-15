6712c0a8f1b55b374e927cd987a325bccbb60269
Clément <clef-men@orange.fr>
2024-10-15 17:12:52 +0200

    Fix make_package.sh.

--- make_package.sh
+++ make_package.sh
@@ -9,7 +9,8 @@ coqfile="_CoqProject.$package"
 makefile="Makefile.$package"
 
 rm -f "$coqfile"
-grep -E "^-" _CoqProject >> "$coqfile"
+grep -E "^-arg" _CoqProject >> "$coqfile"
+echo "-Q theories/$(echo $package | sed -e 's:^zoo::' -e 's:^-::') ${package//-/.}" >> "$coqfile"
 awk "/^#END ${package}$/{b=0} b; /^#BEGIN ${package}$/{b=1}" _CoqProject >> "$coqfile"
 
 coq_makefile -f "$coqfile" -o "$makefile"
