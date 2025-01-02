041de3d067450b6569e5c62a91c6f80ac3e8a0bf
Clément <clef-men@orange.fr>
2025-01-02 02:14:28 +0100

    Add zoo_std/queue_2.v.

---
+++ theories/zoo_std/queue_2.v
@@ -0,0 +1,14 @@
+From zoo Require Import
+  prelude.
+From zoo_std Require Export
+  base
+  queue_2__code.
+From zoo_std Require Import
+  queue_2__types.
+From zoo Require Import
+  options.
+
+#[global] Opaque queue_2_create.
+#[global] Opaque queue_2_is_empty.
+#[global] Opaque queue_2_push.
+#[global] Opaque queue_2_pop.
