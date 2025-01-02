9f65cac39bc35af51f402c7e9e4bb64a2cdd7b67
Clément <clef-men@orange.fr>
2025-01-02 03:01:24 +0100

    Add zoo_saturn/htbl.v.

---
+++ theories/zoo_saturn/htbl.v
@@ -0,0 +1,15 @@
+From zoo Require Import
+  prelude.
+From zoo_saturn Require Export
+  base
+  htbl__code.
+From zoo_saturn Require Import
+  htbl__types.
+From zoo Require Import
+  options.
+
+#[global] Opaque htbl_create.
+#[global] Opaque htbl_find.
+#[global] Opaque htbl_mem.
+#[global] Opaque htbl_add.
+#[global] Opaque htbl_remove.
