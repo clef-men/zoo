52b9ed63ca80bb9872a26b1c049bef229dc5088b
Clément <clef-men@orange.fr>
2025-01-30 11:19:03 +0100

    zoo_saturn/inf_mpmc_queue_1: Verify [pop].

--- theories/zoo_saturn/inf_mpmc_queue_1.v
+++ theories/zoo_saturn/inf_mpmc_queue_1.v
@@ -3,6 +3,8 @@ From zoo Require Import
 From zoo.common Require Import
   countable
   function.
+From zoo.iris.bi Require Import
+  big_op.
 From zoo.iris.base_logic Require Import
   lib.twins
   lib.mono_list
@@ -28,6 +30,7 @@ Implicit Types front back : nat.
 Implicit Types l : location.
 Implicit Types t v : val.
 Implicit Types vs hist : list val.
+Implicit Types slot : optional val.
 Implicit Types slots : nat → optional val.
 Implicit Types η : gname.
 Implicit Types ηs : list gname.
@@ -102,10 +105,22 @@ Section inf_mpmc_queue_1_G.
     ⌜length ηs = i⌝.
   #[local] Definition consumers_auth γ i :=
     consumers_auth' γ.(metadata_consumers) i.
+  #[local] Instance : CustomIpatFormat "consumers_auth" :=
+    "(
+      %ηs{} &
+      Hauth{} &
+      %Hηs{}
+    )".
   #[local] Definition consumers_at γ i Ψ : iProp Σ :=
     ∃ η,
     mono_list_at γ.(metadata_consumers) i η ∗
     saved_pred η Ψ.
+  #[local] Instance : CustomIpatFormat "consumers_at" :=
+    "(
+      %η{} &
+      Hat{} &
+      HΨ{}
+    )".
 
   #[local] Definition tokens_auth' γ_tokens i : iProp Σ :=
     ∃ ηs,
@@ -113,14 +128,32 @@ Section inf_mpmc_queue_1_G.
     ⌜length ηs = i⌝.
   #[local] Definition tokens_auth γ i :=
     tokens_auth' γ.(metadata_tokens) i.
+  #[local] Instance : CustomIpatFormat "tokens_auth" :=
+    "(
+      %ηs{} &
+      Hauth{} &
+      %Hηs{}
+    )".
   #[local] Definition tokens_pending γ i : iProp Σ :=
     ∃ η,
     mono_list_at γ.(metadata_tokens) i η ∗
     oneshot_pending η (DfracOwn 1) ().
+  #[local] Instance : CustomIpatFormat "tokens_pending" :=
+    "(
+      %η{} &
+      Hat{} &
+      Hpending{}
+    )".
   #[local] Definition tokens_done γ i : iProp Σ :=
     ∃ η,
     mono_list_at γ.(metadata_tokens) i η ∗
     oneshot_shot η ().
+  #[local] Instance : CustomIpatFormat "tokens_done" :=
+    "(
+      %η{} &
+      Hat{} &
+      Hshot{}
+    )".
 
   #[local] Definition consumer_au γ ι Ψ : iProp Σ :=
     AU <{
@@ -134,6 +167,15 @@ Section inf_mpmc_queue_1_G.
       Ψ v
     }>.
 
+  #[local] Definition slot_model γ i slot : iProp Σ :=
+    match slot with
+    | Something v =>
+        history_at γ i v
+    | Anything =>
+        tokens_done γ i
+    | Nothing =>
+        True
+    end.
   #[local] Definition inv_inner l γ ι : iProp Σ :=
     ∃ front back hist slots,
     l.[front] ↦ #front ∗
@@ -146,26 +188,20 @@ Section inf_mpmc_queue_1_G.
     tokens_auth γ (front `max` back) ∗
     ( [∗ list] i ∈ seq 0 back,
         tokens_pending γ i
-      ∨ ∃ Ψ v,
+      ∨ ∃ Ψ,
         consumers_at γ i Ψ ∗
-        history_at γ i v ∗
-        Ψ v
+        ( tokens_done γ i
+        ∨ ∃ v,
+          history_at γ i v ∗
+          Ψ v
+        )
     ) ∗
     ( [∗ list] i ∈ seq back (front - back),
       ∃ Ψ,
       consumers_at γ i Ψ ∗
       consumer_au γ ι Ψ
     ) ∗
-    ( ∀ i,
-      match slots i with
-      | Something v =>
-          history_at γ i v
-      | Anything =>
-          tokens_done γ i
-      | Nothing =>
-          True
-      end
-    ).
+    (∀ i, slot_model γ i (slots i)).
   #[local] Instance : CustomIpatFormat "inv_inner" :=
     "(
       %front{} &
@@ -175,7 +211,7 @@ Section inf_mpmc_queue_1_G.
       Hl_front &
       Hl_back &
       >Hdata_model &
-      Hhistory_auth &
+      >Hhistory_auth &
       >%Hhist{} &
       Hmodel₂ &
       Hconsumers_auth &
@@ -184,22 +220,28 @@ Section inf_mpmc_queue_1_G.
       Hwaiters &
       Hslots
     )".
-  Definition inf_mpmc_queue_1_inv t ι : iProp Σ :=
-    ∃ l γ,
-    ⌜t = #l⌝ ∗
+  Definition inv' l γ ι : iProp Σ :=
     meta l nroot γ ∗
     l.[data] ↦□ γ.(metadata_data) ∗
     inf_array_inv γ.(metadata_data) ∗
     inv ι (inv_inner l γ ι).
+  #[local] Instance : CustomIpatFormat "inv'" :=
+    "(
+      #Hmeta &
+      #Hl_data &
+      #Hdata_inv &
+      #Hinv
+    )".
+  Definition inf_mpmc_queue_1_inv t ι : iProp Σ :=
+    ∃ l γ,
+    ⌜t = #l⌝ ∗
+    inv' l γ ι.
   #[local] Instance : CustomIpatFormat "inv" :=
     "(
       %l &
       %γ &
       -> &
-      #Hmeta &
-      #Hl_data &
-      #Hdata_inv &
-      #Hinv
+      (:inv')
     )".
 
   Definition inf_mpmc_queue_1_model t vs : iProp Σ :=
@@ -216,6 +258,21 @@ Section inf_mpmc_queue_1_G.
       Hmodel₁
     )".
 
+  #[local] Instance tokens_pending_timeless γ i :
+    Timeless (tokens_pending γ i).
+  Proof.
+    apply _.
+  Qed.
+  #[local] Instance tokens_done_timeless γ i :
+    Timeless (tokens_done γ i).
+  Proof.
+    apply _.
+  Qed.
+  #[local] Instance slot_model_timeless γ i slot :
+    Timeless (slot_model γ i slot).
+  Proof.
+    rewrite /slot_model. apply _.
+  Qed.
   #[global] Instance inf_mpmc_queue_1_model_timeless t vs :
     Timeless (inf_mpmc_queue_1_model t vs).
   Proof.
@@ -225,6 +282,16 @@ Section inf_mpmc_queue_1_G.
     Persistent (consumers_at γ i Ψ).
   Proof.
     apply _.
+  Qed.
+  #[local] Instance tokens_done_persistent γ i :
+    Persistent (tokens_done γ i).
+  Proof.
+    apply _.
+  Qed.
+  #[local] Instance slot_model_persistent γ i slot :
+    Persistent (slot_model γ i slot).
+  Proof.
+    rewrite /slot_model. apply _.
   Qed.
   #[global] Instance inf_mpmc_queue_1_inv_persistent t ι :
     Persistent (inf_mpmc_queue_1_inv t ι).
@@ -248,6 +315,27 @@ Section inf_mpmc_queue_1_G.
     apply twins_update'.
   Qed.
 
+  #[local] Lemma history_at_valid γ hist i v :
+    history_auth γ hist -∗
+    history_at γ i v -∗
+    ⌜hist !! i = Some v⌝.
+  Proof.
+    apply mono_list_at_valid.
+  Qed.
+  #[local] Lemma history_at_agree γ i v1 v2 :
+    history_at γ i v1 -∗
+    history_at γ i v2 -∗
+    ⌜v1 = v2⌝.
+  Proof.
+    apply mono_list_at_agree.
+  Qed.
+  #[local] Lemma history_at_get {γ hist} i v :
+    hist !! i = Some v →
+    history_auth γ hist ⊢
+    history_at γ i v.
+  Proof.
+    apply mono_list_at_get.
+  Qed.
   #[local] Lemma history_update {γ hist} v :
     history_auth γ hist ⊢ |==>
       history_auth γ (hist ++ [v]) ∗
@@ -260,17 +348,76 @@ Section inf_mpmc_queue_1_G.
     iSteps.
   Qed.
 
+  #[local] Lemma consumers_at_valid γ i j Ψ :
+    consumers_auth γ i -∗
+    consumers_at γ j Ψ -∗
+    ⌜j < i⌝.
+  Proof.
+    iIntros "(:consumers_auth) (:consumers_at)".
+    iDestruct (mono_list_at_valid with "Hauth Hat") as %?Hj%lookup_lt_Some.
+    iSteps.
+  Qed.
+  #[local] Lemma consumers_at_agree γ i Ψ1 Ψ2 v :
+    consumers_at γ i Ψ1 -∗
+    ▷ consumers_at γ i Ψ2 -∗
+    ▷ Ψ2 v -∗
+    ▷^2 Ψ1 v.
+  Proof.
+    iIntros "(:consumers_at =1) (:consumers_at =2) HΨ !>".
+    iDestruct (mono_list_at_agree with "Hat1 Hat2") as %<-.
+    iDestruct (saved_pred_agree v with "HΨ1 HΨ2") as "Heq".
+    iModIntro. iRewrite "Heq". iSteps.
+  Qed.
+  #[local] Lemma consumers_update {γ i} Ψ :
+    consumers_auth γ i ⊢ |==>
+      consumers_auth γ (S i) ∗
+      consumers_at γ i Ψ.
+  Proof.
+    iIntros "(:consumers_auth)".
+    iMod (saved_pred_alloc Ψ) as "(%η & HΨ)".
+    iMod (mono_list_update_snoc with "Hauth") as "Hauth".
+    iDestruct (mono_list_at_get with "Hauth") as "#Hat".
+    { rewrite list_lookup_middle //. }
+    iFrame. rewrite length_app. iSteps.
+  Qed.
+
+  #[local] Lemma tokens_pending_exclusive γ i :
+    tokens_pending γ i -∗
+    tokens_pending γ i -∗
+    False.
+  Proof.
+    iIntros "(:tokens_pending =1) (:tokens_pending =2)".
+    iDestruct (mono_list_at_agree with "Hat1 Hat2") as %<-.
+    iApply (oneshot_pending_exclusive with "Hpending1 Hpending2").
+  Qed.
+  #[local] Lemma tokens_pending_done γ i :
+    tokens_pending γ i -∗
+    tokens_done γ i -∗
+    False.
+  Proof.
+    iIntros "(:tokens_pending =1) (:tokens_done =2)".
+    iDestruct (mono_list_at_agree with "Hat1 Hat2") as %<-.
+    iApply (oneshot_pending_shot with "Hpending1 Hshot2").
+  Qed.
   #[local] Lemma tokens_update {γ} i :
     tokens_auth γ i ⊢ |==>
       tokens_auth γ (S i) ∗
       tokens_pending γ i.
   Proof.
-    iIntros "(%ηs & Hauth & %Hηs)".
+    iIntros "(:tokens_auth)".
     iMod oneshot_alloc as "(%η & Hpending)".
     iMod (mono_list_update_snoc with "Hauth") as "Hauth".
     iDestruct (mono_list_at_get with "Hauth") as "#Hat".
     { rewrite list_lookup_middle //. }
     iFrame. rewrite length_app. iSteps.
+  Qed.
+  #[local] Lemma tokens_pending_update γ i :
+    tokens_pending γ i ⊢ |==>
+    tokens_done γ i.
+  Proof.
+    iIntros "(:tokens_pending)".
+    iMod (oneshot_update_shot with "Hpending") as "Hshot".
+    iSteps.
   Qed.
 
   Opaque consumers_auth'.
@@ -323,8 +470,7 @@ Section inf_mpmc_queue_1_G.
       - rewrite Nat.max_r //.
 
         iMod (tokens_update with "Htokens_auth") as "(Htokens_auth & Htokens_at)".
-        iDestruct (big_sepL_snoc with "[$Hpast $Htokens_at]") as "Hpast".
-        rewrite -seq_S.
+        iDestruct (big_sepL_seq_snoc_2 with "Hpast [$Htokens_at]") as "Hpast".
 
         iSplitL.
         { iExists front1, (S back1). iFrame.
@@ -341,15 +487,14 @@ Section inf_mpmc_queue_1_G.
         { rewrite Hvs length_drop. lia. }
         assert (front1 - back1 = S (front1 - S back1)) as ->; first lia.
         destruct (Nat.lt_exists_pred 0 (front1 - back1)) as (δ & ? & _); first lia.
-        rewrite -cons_seq. iDestruct "Hwaiters" as "((%Ψ & #Hconsumers_at & HΨ) & Hwaiters)".
+        iDestruct (big_sepL_seq_cons_1 with "Hwaiters") as "((%Ψ & #Hconsumers_at & HΨ) & Hwaiters)".
 
         iMod "HΨ" as "(% & Hmodel₁ & _ & HΨ)".
         iDestruct (model_agree with "Hmodel₁ Hmodel₂") as %->.
         iMod (model_update [] with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
         iMod ("HΨ" with "[$Hmodel₁ //]") as "HΨ".
 
-        iDestruct (big_sepL_snoc with "[$Hpast HΨ]") as "Hpast"; first iSteps.
-        rewrite -seq_S.
+        iDestruct (big_sepL_seq_snoc_2 with "Hpast [HΨ]") as "Hpast"; first iSteps.
 
         iSplitL.
         { iFrame.
@@ -364,9 +509,7 @@ Section inf_mpmc_queue_1_G.
 
     awp_apply (inf_array_set_spec with "Hdata_inv") without "HΦ"; first lia.
     iInv "Hinv" as "(:inv_inner =2)".
-    iAaccIntro with "Hdata_model".
-    { iIntros. iFrame. done. }
-    iIntros "Hdata_model !>".
+    iAaccIntro with "Hdata_model"; first auto with iFrame. iIntros "Hdata_model !>".
     iSplitL.
     { repeat iExists _.
       rewrite Nat2Z.id -(fn_compose_insert _ _ _ (Something v)).
@@ -375,6 +518,144 @@ Section inf_mpmc_queue_1_G.
       case_decide; first subst; iSteps.
     }
     iSteps.
+  Qed.
+
+  #[local] Lemma inf_mpmc_queue_1_pop_0_spec l γ ι front Ψ :
+    {{{
+      inv' l γ ι ∗
+      consumers_at γ front Ψ ∗
+      tokens_pending γ front
+    }}}
+      inf_mpmc_queue_1_pop_0 #l #front
+    {{{ v,
+      RET v;
+      Ψ v
+    }}}.
+  Proof.
+    iIntros "%Φ ((:inv') & #Hconsumers_at & Htokens_pending) HΦ".
+
+    iLöb as "HLöb".
+
+    wp_rec credit:"H£". wp_load.
+
+    awp_apply (inf_array_get_spec with "Hdata_inv") without "Htokens_pending H£ HΦ"; first lia.
+    iInv "Hinv" as "(:inv_inner =1)".
+    iAaccIntro with "Hdata_model"; first auto with iFrame. iIntros "Hdata_model".
+    iAssert (▷ slot_model γ front (slots1 front))%I with "[Hslots]" as "#>Hslot"; first iSteps.
+    iSplitL; first iSteps.
+    iIntros "!> _ (Htokens_pending & H£ & HΦ) {%}".
+
+    rewrite Nat2Z.id /=. destruct (slots1 front) as [| | v].
+
+    - iStep 7. iModIntro.
+      wp_apply ("HLöb" with "Htokens_pending HΦ").
+
+    - iDestruct (tokens_pending_done with "Htokens_pending Hslot") as %[].
+
+    - wp_load.
+
+      awp_apply (inf_array_set_spec with "Hdata_inv") without "H£"; first lia.
+      iInv "Hinv" as "(:inv_inner =2)".
+      iAaccIntro with "Hdata_model"; first auto with iFrame. iIntros "Hdata_model".
+      iDestruct (history_at_valid with "Hhistory_auth Hslot") as %Hhist2_lookup.
+      opose proof* lookup_lt_Some as Hfront; first done.
+      iDestruct (big_sepL_seq_lookup_acc' front with "Hpast") as "([>Htokens_pending_ | (%Ψ_ & Hconsumers_at_ & [>Htokens_done | (%v_ & >Hhistory_at & HΨ)])] & Hpast)"; first lia.
+      { iDestruct (tokens_pending_exclusive with "Htokens_pending Htokens_pending_") as %[]. }
+      { iDestruct (tokens_pending_done with "Htokens_pending Htokens_done") as %[]. }
+      iDestruct (history_at_agree with "Hslot Hhistory_at") as %<-.
+      iDestruct (consumers_at_agree with "Hconsumers_at Hconsumers_at_ HΨ") as "HΨ".
+      iMod (tokens_pending_update with "Htokens_pending") as "#Htokens_done".
+      iDestruct ("Hpast" with "[]") as "Hpast"; first iSteps.
+      iSplitR "HΨ HΦ".
+      { rewrite Nat2Z.id -(fn_compose_insert _ _ _ Anything).
+        iFrame. iSteps.
+        rewrite function.fn_lookup_insert.
+        case_decide; first subst; iSteps.
+      }
+      iIntros "!> _ H£ {%}".
+
+      iMod (lc_fupd_elim_later with "H£ HΨ") as "HΨ".
+      iSteps.
+  Qed.
+  Lemma inf_mpmc_queue_1_pop_spec t ι :
+    <<<
+      inf_mpmc_queue_1_inv t ι
+    | ∀∀ vs,
+      inf_mpmc_queue_1_model t vs
+    >>>
+      inf_mpmc_queue_1_pop t @ ↑ι
+    <<<
+      ∃∃ v vs',
+      ⌜vs = v :: vs'⌝ ∗
+      inf_mpmc_queue_1_model t vs'
+    | RET v;
+      True
+    >>>.
+  Proof.
+    iIntros "!> %Φ (:inv) HΦ".
+
+    wp_rec. wp_pures.
+
+    wp_bind (FAA _ _).
+    wp_apply (wp_wand _ _
+      ( λ res,
+        ∃ front,
+        ⌜res = #front⌝ ∗
+        consumers_at γ front Φ ∗
+        tokens_pending γ front
+      )%I
+      with "[HΦ]"
+    ) as (res) "(%front & -> & Hconsumers_at & Htokens_pending)".
+    { iInv "Hinv" as "(:inv_inner)".
+      wp_faa.
+      destruct (decide (front < back)) as [Hfront1 | Hfront1].
+
+      - rewrite Nat.max_r; first lia.
+        destruct (lookup_lt_is_Some_2 hist front) as (v & Hhist_lookup); first lia.
+        erewrite drop_S; last done.
+
+        iDestruct (history_at_get with "Hhistory_auth") as "#Hhistory_at"; first done.
+        iDestruct (big_sepL_seq_lookup_acc front with "Hpast") as "([$ | (%Ψ & Hconsumers_at & _)] & Hpast)"; first lia; last first.
+        { iDestruct (consumers_at_valid with "Hconsumers_auth Hconsumers_at") as %?. lia. }
+        iMod (consumers_update Φ with "Hconsumers_auth") as "(Hconsumers_auth & #Hconsumers_at)".
+
+        iMod "HΦ" as "(%vs & (:model) & _ & HΦ)". injection Heq as <-.
+        iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
+        iDestruct (model_agree with "Hmodel₁ Hmodel₂") as %->.
+        iMod (model_update (drop (S front) hist) with "Hmodel₁ Hmodel₂") as "(Hmodel₁ & Hmodel₂)".
+        iMod ("HΦ" with "[Hmodel₁] [//]") as "HΦ"; first iSteps.
+
+        iSplitL.
+        { iFrame. iSteps.
+          rewrite Nat.max_r //.
+          assert (S front - back = 0) as -> by lia.
+          iSteps.
+        }
+        iSteps.
+
+      - rewrite Nat.max_l; first lia.
+
+        iMod (consumers_update Φ with "Hconsumers_auth") as "(Hconsumers_auth & #Hconsumers_at)".
+        iMod (tokens_update with "Htokens_auth") as "(Htokens_auth & $)".
+        iDestruct (big_sepL_seq_snoc_2 with "Hwaiters [HΦ]") as "Hwaiters".
+        { rewrite -Nat.le_add_sub; first lia.
+          iSteps. rewrite /consumer_au. iAuIntro.
+          iApply (aacc_aupd_commit with "HΦ"); first done. iIntros "%vs (:model)". injection Heq as <-.
+          iDestruct (meta_agree with "Hmeta _Hmeta") as %<-. iClear "_Hmeta".
+          iAaccIntro with "Hmodel₁"; iSteps.
+        }
+        rewrite -Nat.sub_succ_l; first lia.
+
+        iSplitL.
+        { iFrame. iSteps.
+          rewrite !skipn_all2; [lia.. |].
+          rewrite Nat.max_l; first lia.
+          iSteps.
+        }
+        iSteps.
+    }
+
+    wp_smart_apply (inf_mpmc_queue_1_pop_0_spec with "[$Hconsumers_at $Htokens_pending]"); iSteps.
   Qed.
 End inf_mpmc_queue_1_G.
 
