3305c3153f2bfa6fff2819f4080ef99aa3e98b9d
Clément <clef-men@orange.fr>
2025-04-15 17:54:26 +0200

    zoo_saturn: Specify ws_deque_1 and verify ws_deque_2.

--- theories/zoo_saturn/ws_deque_1.v
+++ theories/zoo_saturn/ws_deque_1.v
@@ -1,5 +1,11 @@
 From zoo Require Import
   prelude.
+From zoo.language Require Import
+  notations.
+From zoo.diaframe Require Import
+  diaframe.
+From zoo_std Require Import
+  option.
 From zoo_saturn Require Export
   base
   ws_deque_1__code.
@@ -8,7 +14,129 @@ From zoo_saturn Require Import
 From zoo Require Import
   options.
 
+Implicit Types v t : val.
+Implicit Types vs : list val.
+
+Class WsDeque1G Σ `{zoo_G : !ZooG Σ} := {
+}.
+
+Definition ws_deque_1_Σ := #[
+].
+#[global] Instance subG_ws_deque_1_Σ Σ `{zoo_G : !ZooG Σ} :
+  subG ws_deque_1_Σ Σ →
+  WsDeque1G Σ .
+Proof.
+  (* solve_inG. *)
+Qed.
+
+Section ws_deque_1_G.
+  Context `{ws_deque_1_G : WsDeque1G Σ}.
+
+  Definition ws_deque_1_inv t (ι : namespace) : iProp Σ.
+  Proof. Admitted.
+
+  Definition ws_deque_1_model t vs : iProp Σ.
+  Proof. Admitted.
+
+  Definition ws_deque_1_owner t : iProp Σ.
+  Proof. Admitted.
+
+  #[global] Instance ws_deque_1_model_timeless t model :
+    Timeless (ws_deque_1_model t model).
+  Proof.
+  Admitted.
+  #[global] Instance ws_deque_1_owner_timeless t :
+    Timeless (ws_deque_1_owner t).
+  Proof.
+  Admitted.
+  #[global] Instance ws_deque_1_inv_persistent t ι :
+    Persistent (ws_deque_1_inv t ι).
+  Proof.
+  Admitted.
+
+  Lemma ws_deque_1_owner_exclusive t :
+    ws_deque_1_owner t -∗
+    ws_deque_1_owner t -∗
+    False.
+  Proof.
+  Admitted.
+
+  Lemma ws_deque_1_create_spec ι :
+    {{{
+      True
+    }}}
+      ws_deque_1_create ()
+    {{{ t,
+      RET t;
+      ws_deque_1_inv t ι ∗
+      ws_deque_1_model t [] ∗
+      ws_deque_1_owner t
+    }}}.
+  Proof.
+  Admitted.
+
+  Lemma ws_deque_1_push_spec t ι v :
+    <<<
+      ws_deque_1_inv t ι ∗
+      ws_deque_1_owner t
+    | ∀∀ vs,
+      ws_deque_1_model t vs
+    >>>
+      ws_deque_1_push t v @ ↑ι
+    <<<
+      ws_deque_1_model t (vs ++ [v])
+    | RET ();
+      ws_deque_1_owner t
+    >>>.
+  Proof.
+  Admitted.
+
+  Lemma ws_deque_1_steal_spec t ι :
+    <<<
+      ws_deque_1_inv t ι
+    | ∀∀ vs,
+      ws_deque_1_model t vs
+    >>>
+      ws_deque_1_steal t @ ↑ι
+    <<<
+      ws_deque_1_model t (tail vs)
+    | RET head vs;
+      True
+    >>>.
+  Proof.
+  Admitted.
+
+  Lemma ws_deque_1_pop_spec t ι :
+    <<<
+      ws_deque_1_inv t ι ∗
+      ws_deque_1_owner t
+    | ∀∀ vs,
+      ws_deque_1_model t vs
+    >>>
+      ws_deque_1_pop t @ ↑ι
+    <<<
+      ∃∃ o,
+      match o with
+      | None =>
+          ⌜vs = []⌝ ∗
+          ws_deque_1_model t []
+      | Some v =>
+          ∃ vs',
+          ⌜vs = vs' ++ [v]⌝ ∗
+          ws_deque_1_model t vs'
+      end
+    | RET o;
+      ws_deque_1_owner t
+    >>>.
+  Proof.
+  Admitted.
+End ws_deque_1_G.
+
 #[global] Opaque ws_deque_1_create.
 #[global] Opaque ws_deque_1_push.
 #[global] Opaque ws_deque_1_pop.
 #[global] Opaque ws_deque_1_steal.
+
+#[global] Opaque ws_deque_1_inv.
+#[global] Opaque ws_deque_1_model.
+#[global] Opaque ws_deque_1_owner.
--- theories/zoo_saturn/ws_deque_2.v
+++ theories/zoo_saturn/ws_deque_2.v
@@ -1,14 +1,187 @@
 From zoo Require Import
   prelude.
+From zoo.common Require Import
+  list.
+From zoo.language Require Import
+  notations.
+From zoo.diaframe Require Import
+  diaframe.
+From zoo_std Require Import
+  option.
 From zoo_saturn Require Export
   base
   ws_deque_2__code.
 From zoo_saturn Require Import
-  ws_deque_2__types.
+  ws_deque_1.
 From zoo Require Import
   options.
 
+Implicit Types v t : val.
+
+Class WsDeque2G Σ `{zoo_G : !ZooG Σ} := {
+  #[local] ws_deque_2_G :: WsDeque1G Σ ;
+}.
+
+Definition ws_deque_2_Σ := #[
+  ws_deque_1_Σ
+].
+#[global] Instance subG_ws_deque_2_Σ Σ `{zoo_G : !ZooG Σ} :
+  subG ws_deque_2_Σ Σ →
+  WsDeque2G Σ .
+Proof.
+  solve_inG.
+Qed.
+
+Section ws_deque_2_G.
+  Context `{ws_deque_2_G : WsDeque2G Σ}.
+
+  Definition ws_deque_2_inv t :=
+    ws_deque_1_inv t.
+
+  Definition ws_deque_2_model t vs : iProp Σ :=
+      ∃ slots,
+      ws_deque_1_model t (#@{location} <$> slots) ∗
+      [∗ list] slot; v ∈ slots; vs,
+        slot ↦ᵣ v.
+
+  Definition ws_deque_2_owner t :=
+    ws_deque_1_owner t.
+
+  #[global] Instance ws_deque_2_model_timeless t model :
+    Timeless (ws_deque_2_model t model).
+  Proof.
+    apply _.
+  Qed.
+  #[global] Instance ws_deque_2_owner_timeless t :
+    Timeless (ws_deque_2_owner t).
+  Proof.
+    apply _.
+  Qed.
+  #[global] Instance ws_deque_2_inv_persistent t ι :
+    Persistent (ws_deque_2_inv t ι).
+  Proof.
+    apply _.
+  Qed.
+
+  Lemma ws_deque_2_owner_exclusive t :
+    ws_deque_2_owner t -∗
+    ws_deque_2_owner t -∗
+    False.
+  Proof.
+    apply ws_deque_1_owner_exclusive.
+  Qed.
+
+  Lemma ws_deque_2_create_spec ι :
+    {{{
+      True
+    }}}
+      ws_deque_2_create ()
+    {{{ t,
+      RET t;
+      ws_deque_2_inv t ι ∗
+      ws_deque_2_model t [] ∗
+      ws_deque_2_owner t
+    }}}.
+  Proof.
+    iIntros "%Φ _ HΦ".
+    wp_apply (ws_deque_1_create_spec with "[//]") as (t) "(#Hinv & Hmodel & Howner)".
+    iSteps. iExists []. iSteps.
+  Qed.
+
+  Lemma ws_deque_2_push_spec t ι v :
+    <<<
+      ws_deque_2_inv t ι ∗
+      ws_deque_2_owner t
+    | ∀∀ model,
+      ws_deque_2_model t model
+    >>>
+      ws_deque_2_push t v @ ↑ι
+    <<<
+      ws_deque_2_model t (model ++ [v])
+    | RET ();
+      ws_deque_2_owner t
+    >>>.
+  Proof.
+    iIntros "%Φ (#Hinv & Howner) HΦ".
+    wp_rec. wp_ref slot as "Hslot".
+    awp_apply (ws_deque_1_push_spec with "[$Hinv $Howner]").
+    iApply (aacc_aupd_commit with "HΦ"); first done. iIntros "%vs (%slots & Hmodel & Hslots)".
+    iAaccIntro with "Hmodel"; iIntros "Hmodel !>"; first iSteps.
+    iSplitL; last iSteps. rewrite -fmap_snoc. iExists _. iFrameSteps.
+  Qed.
+
+  Lemma ws_deque_2_steal_spec t ι :
+    <<<
+      ws_deque_2_inv t ι
+    | ∀∀ model,
+      ws_deque_2_model t model
+    >>>
+      ws_deque_2_steal t @ ↑ι
+    <<<
+      ws_deque_2_model t (tail model)
+    | RET head model;
+      True
+    >>>.
+  Proof.
+    iIntros "%Φ #Hinv HΦ".
+    wp_rec.
+    awp_smart_apply (ws_deque_1_steal_spec with "Hinv").
+    iApply (aacc_aupd_commit with "HΦ"); first done. iIntros "%vs (%slots & Hmodel & Hslots)".
+    iAaccIntro with "Hmodel"; iIntros "Hmodel !>"; first iSteps.
+    destruct slots as [| slot slots], vs as [| v vs]; try done.
+    - iSplitL; last iSteps. iExists _. auto.
+    - iSteps.
+  Qed.
+
+  Lemma ws_deque_2_pop_spec t ι :
+    <<<
+      ws_deque_2_inv t ι ∗
+      ws_deque_2_owner t
+    | ∀∀ model,
+      ws_deque_2_model t model
+    >>>
+      ws_deque_2_pop t @ ↑ι
+    <<<
+      ∃∃ o,
+      match o with
+      | None =>
+          ⌜model = []⌝ ∗
+          ws_deque_2_model t []
+      | Some v =>
+          ∃ model',
+          ⌜model = model' ++ [v]⌝ ∗
+          ws_deque_2_model t model'
+      end
+    | RET o;
+      ws_deque_2_owner t
+    >>>.
+  Proof.
+    iIntros "%Φ (#Hinv & Howner) HΦ".
+    wp_rec.
+    awp_smart_apply (ws_deque_1_pop_spec with "[$Hinv $Howner]").
+    iApply (aacc_aupd_commit with "HΦ"); first done. iIntros "%vs (%slots & Hmodel & Hslots)".
+    iAaccIntro with "Hmodel"; first iSteps. iIntros ([w |]).
+    - iIntros "(%ws & %Heq & Hmodel) !>".
+      destruct (rev_elim slots) as [-> | (slots' & slot & ->)].
+      { rewrite fmap_nil in Heq. edestruct app_cons_not_nil. done. }
+      rewrite fmap_app app_inj_tail_iff in Heq. destruct Heq as (<- & <-).
+      destruct (rev_elim vs) as [-> | (vs' & v & ->)].
+      { iDestruct (big_sepL2_nil_inv_r with "Hslots") as %?.
+        edestruct app_cons_not_nil. done.
+      }
+      iDestruct (big_sepL2_snoc with "Hslots") as "(Hslots & Hslot)".
+      iExists (Some v). iSteps.
+    - iIntros "(%Heq & Hmodel) !>".
+      apply fmap_nil_inv in Heq as ->. iDestruct (big_sepL2_nil_inv_l with "Hslots") as %->.
+      iExists None. iSplitL; last iSteps. iSplit; first iSteps. iExists _. auto.
+  Qed.
+End ws_deque_2_G.
+
 #[global] Opaque ws_deque_2_create.
 #[global] Opaque ws_deque_2_push.
-#[global] Opaque ws_deque_2_pop.
 #[global] Opaque ws_deque_2_steal.
+#[global] Opaque ws_deque_2_pop.
+
+#[global] Opaque ws_deque_2_inv.
+#[global] Opaque ws_deque_2_model.
+#[global] Opaque ws_deque_2_owner.
