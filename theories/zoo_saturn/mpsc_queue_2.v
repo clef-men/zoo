291ee9c03413aee66a5305faab059f4ff275a837
Clément <clef-men@orange.fr>
2025-06-23 17:19:08 +0200

    zoo_saturn/mpsc_queue_2: Add [mpsc_queue_2_model_exclusive].

--- theories/zoo_saturn/mpsc_queue_2.v
+++ theories/zoo_saturn/mpsc_queue_2.v
@@ -108,11 +108,11 @@ Section mpsc_queue_2_G.
     model₁ γ vs.
   #[local] Instance : CustomIpatFormat "model" :=
     "(
-      %l_ &
-      %γ_ &
-      %Heq &
-      Hmeta_ &
-      Hmodel₁
+      %l{;_} &
+      %γ{;_} &
+      %Heq{} &
+      Hmeta_{} &
+      Hmodel₁{_{}}
     )".
 
   Definition mpsc_queue_2_consumer t : iProp Σ :=
@@ -156,6 +156,13 @@ Section mpsc_queue_2_G.
   Proof.
     apply twins_alloc'.
   Qed.
+  #[local] Lemma model₁_exclusive γ vs1 vs2 :
+    model₁ γ vs1 -∗
+    model₁ γ vs2 -∗
+    False.
+  Proof.
+    apply twins_twin1_exclusive.
+  Qed.
   #[local] Lemma model_agree γ vs1 vs2 :
     model₁ γ vs1 -∗
     model₂ γ vs2 -∗
@@ -194,6 +201,16 @@ Section mpsc_queue_2_G.
       front₂ γ front.
   Proof.
     apply twins_update'.
+  Qed.
+
+  Lemma mpsc_queue_2_model_exclusive t vs1 vs2 :
+    mpsc_queue_2_model t vs1 -∗
+    mpsc_queue_2_model t vs2 -∗
+    False.
+  Proof.
+    iIntros "(:model =1) (:model =2)". simplify.
+    iDestruct (meta_agree with "Hmeta_1 Hmeta_2") as %->.
+    iApply (model₁_exclusive with "Hmodel₁_1 Hmodel₁_2").
   Qed.
 
   Lemma mpsc_queue_2_consumer_exclusive t :
