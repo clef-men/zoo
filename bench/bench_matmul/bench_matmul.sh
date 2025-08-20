01d1014202057e0d3195af5d92dd668df64cf21a
Clément <clef-men@orange.fr>
2025-08-20 17:30:26 +0200

    Add bench_matmul. More parallelization is possible.

---
+++ bench/bench_matmul/bench_matmul.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -eou pipefail
+
+inputs="500"
+
+for input in $inputs; do
+  hyperfine \
+    "$@" \
+    --warmup 10 \
+    --runs 20 \
+    -L method parabs,domainslib \
+    --command-name "method:{method} size:$input" \
+    "./_build/default/bench/bench_matmul/bench_matmul.exe {method} $input"
+done
