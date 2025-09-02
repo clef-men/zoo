b7b341a79b6fb0965d209c11aab2026470edb4cf
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-09-03 00:22:40 +0200

    bench_fibonacci: plotting scripts

---
+++ bench/bench_fibonacci/gen_plot_data.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+set -eou pipefail
+
+input="35"
+cutoffs="5 7 10 12 15 17 20 22 25 27 30 32 35"
+impls="sequential parabs domainslib moonpool-fifo moonpool-ws"
+prog="./_build/default/bench/bench_fibonacci/bench_fibonacci.exe"
+
+dune build bench/bench_fibonacci/bench_fibonacci.exe
+
+DOMAINS=$(($EXTRA_DOMAINS + 1))
+outfile=bench/bench_fibonacci/data/fibonacci_plot_$DOMAINS.data
+
+rm -f $outfile
+
+printf "%s" "# cutoff" >> $outfile
+for impl in $impls
+do
+    printf ", %s" $impl >> $outfile
+done
+echo >> $outfile
+
+for cutoff in $cutoffs
+do
+    printf "%d " $cutoff >> $outfile
+    for impl in $impls
+    do
+        hyperfine --runs 10 "$prog $impl $cutoff $input" --export-json /tmp/out.json
+        printf "%g " $(cat /tmp/out.json | jq .results[0].mean) >> $outfile
+    done
+    echo >> $outfile
+done
+
+echo "Data written to: $outfile"
