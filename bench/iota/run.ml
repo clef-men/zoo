0d2958679c1d1db3da00744a781f173be1fa38f2
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-09-05 18:18:29 +0200

    iota: an explicit CUTOFF parameter

--- bench/iota/run.ml
+++ bench/iota/run.ml
@@ -3,10 +3,10 @@ open Bench
 module Make
   (Pool : Pool.S)
 = struct
-  let main sz ctx =
+  let main ~sz ?cutoff ctx =
     let arr = Array.make sz 0 in
     for _ = 1 to 100 do
-      Pool.for_ ctx ~beg:0 ~end_:sz @@ fun _ctx i ->
+      Pool.for_ ctx ?chunk:cutoff ~beg:0 ~end_:sz @@ fun _ctx i ->
         Array.unsafe_set arr i i
     done
 end
@@ -21,9 +21,14 @@ let num_domains =
   let default = Domain.recommended_domain_count () - 1 in
   Utils.get_int_param "EXTRA_DOMAINS" ~default
 
+let cutoff =
+  match Utils.get_int_param "CUTOFF" ~default:0 with
+    | 0 -> None
+    | n -> assert (n > 0); Some n
+
 let () =
   let (module Pool) = pool in
   let module M = Make(Pool) in
   let pool = Pool.create ~num_domains () in
-  let _ = Pool.run pool (M.main size) in
+  let _ = Pool.run pool (M.main ~sz:size ?cutoff) in
   Pool.kill pool
