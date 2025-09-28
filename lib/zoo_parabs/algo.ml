346ab6658312bfe7daa4f444eb0c9d80ba72569d
Clément <clef-men@orange.fr>
2025-09-28 18:55:50 +0200

    zoo_parabs/algo: Add [fold] and [find].

--- lib/zoo_parabs/algo.ml
+++ lib/zoo_parabs/algo.ml
@@ -4,13 +4,14 @@ let for_ ctx beg end_ chunk fn =
   let sz = end_ - beg in
   let num_chunk = sz / chunk in
   let rest = sz mod chunk in
-  let cnt = Atomic.make sz in
+  let num_task = num_chunk + Bool.to_int (rest != 0) in
+  let num_pending = Atomic.make num_task in
   for i = 0 to num_chunk - 1 do
     Pool.async_silent ctx (fun ctx ->
       for k = i * chunk to i * chunk + chunk - 1 do
         fn ctx k
       done ;
-      Atomic.fetch_and_add cnt (- chunk) |> ignore
+      Atomic.decr num_pending
     )
   done ;
   if rest != 0 then
@@ -18,26 +19,85 @@ let for_ ctx beg end_ chunk fn =
       for k = num_chunk * chunk to end_ - 1 do
         fn ctx k
       done ;
-      Atomic.fetch_and_add cnt (- rest) |> ignore
+      Atomic.decr num_pending
     ) ;
-  Pool.wait_until ctx (fun () -> Atomic.get cnt == 0)
+  Pool.wait_until ctx (fun () -> Atomic.get num_pending == 0)
 
 let divide ctx beg end_ fn =
   let num_dom = Pool.size ctx + 1 in
   let sz = end_ - beg in
   let chunk = sz / num_dom in
   let rest = sz mod num_dom in
-  let num_chunk = num_dom + Bool.to_int (rest != 0) in
-  let cnt = Atomic.make num_chunk in
+  let num_task = num_dom + Bool.to_int (rest != 0) in
+  let num_pending = Atomic.make num_task in
   for i = 0 to num_dom - 1 do
     Pool.async_silent ctx (fun ctx ->
       fn ctx (i * chunk) chunk ;
-      Atomic.decr cnt
+      Atomic.decr num_pending
     )
   done ;
   if rest != 0 then
     Pool.async_silent ctx (fun ctx ->
       fn ctx (num_dom * chunk) rest ;
-      Atomic.decr cnt
+      Atomic.decr num_pending
     ) ;
-  Pool.wait_until ctx (fun () -> Atomic.get cnt == 0)
+  Pool.wait_until ctx (fun () -> Atomic.get num_pending == 0)
+
+let rec fold ctx op body acc beg end_ =
+  if beg == end_ then
+    acc
+  else
+    let acc = op acc (body ctx beg) in
+    let beg = beg + 1 in
+    fold ctx op body acc beg end_
+let fold ctx beg end_ chunk op body zero =
+  let sz = end_ - beg in
+  let num_chunk = sz / chunk in
+  let rest = sz mod chunk in
+  let num_task = num_chunk + Bool.to_int (rest != 0) in
+  let num_pending = Atomic.make num_task in
+  let sums = Array.make num_task zero in
+  for i = 0 to num_chunk - 1 do
+    Pool.async_silent ctx (fun ctx ->
+      let sum = fold ctx op body zero (i * chunk) ((i + 1) * chunk) in
+      Array.unsafe_set sums i sum ;
+      Atomic.decr num_pending
+    )
+  done ;
+  if rest != 0 then
+    Pool.async_silent ctx (fun ctx ->
+      let sum = fold ctx op body zero (num_chunk * chunk) end_ in
+      Array.unsafe_set sums num_chunk sum ;
+      Atomic.decr num_pending
+    ) ;
+  Pool.wait_until ctx (fun () -> Atomic.get num_pending == 0) ;
+  Array.foldl op zero sums
+
+let rec find ctx beg end_ pred found =
+  if beg != end_ && !found != None then (
+    if pred ctx beg then
+      found := Some beg
+    else
+      let beg = beg + 1 in
+      find ctx beg end_ pred found
+  )
+let find ctx beg end_ chunk pred =
+  let sz = end_ - beg in
+  let num_chunk = sz / chunk in
+  let rest = sz mod chunk in
+  let num_task = num_chunk + Bool.to_int (rest != 0) in
+  let num_pending = Atomic.make num_task in
+  let found = ref None in
+  for i = 0 to num_chunk - 1 do
+    Pool.async_silent ctx (fun ctx ->
+      find ctx (i * chunk) ((i + 1) * chunk) pred found ;
+      Atomic.decr num_pending
+    )
+  done ;
+  if rest != 0 then
+    Pool.async_silent ctx (fun ctx ->
+      find ctx (num_chunk * chunk) end_ pred found ;
+      Atomic.decr num_pending
+    ) ;
+  Pool.wait_until ctx (fun () -> Atomic.get num_pending == 0) ;
+  !found
--- lib/zoo_parabs/algo.mli
+++ lib/zoo_parabs/algo.mli
@@ -1,7 +1,34 @@
 [@@@zoo.ignore]
 
 val for_ :
-  Pool.context -> int -> int -> int -> (Pool.context -> int -> unit) -> unit
+  Pool.context ->
+  int ->
+  int ->
+  int ->
+  (Pool.context -> int -> unit) ->
+  unit
 
 val divide :
-  Pool.context -> int -> int -> (Pool.context -> int -> int -> unit) -> unit
+  Pool.context ->
+  int ->
+  int ->
+  (Pool.context -> int -> int -> unit) ->
+  unit
+
+val fold :
+  Pool.context ->
+  int ->
+  int ->
+  int ->
+  ('a -> 'a -> 'a) ->
+  (Pool.context -> int -> 'a) ->
+  'a ->
+  'a
+
+val find :
+  Pool.context ->
+  int ->
+  int ->
+  int ->
+  (Pool.context -> int -> bool) ->
+  int option
