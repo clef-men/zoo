f82511c26b9b228ee4a274c99bb95860ef56c1f2
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-10-08 08:08:12 +0200

    refresh Fibonacci data

--- bench/fibonacci/gen_plot_data_cutoff.sh
+++ bench/fibonacci/gen_plot_data_cutoff.sh
@@ -2,11 +2,12 @@
 
 set -eou pipefail
 
+EXTRA_DOMAINS=6
 benchname="fibonacci"
 input="35"
 cutoffs="5 7 10 12 15 17 20 22 25 27 30 32 35"
 impls="sequential parabs domainslib moonpool-fifo moonpool-ws"
-prog="./_build/default/bench/$benchname/run.exe"
+prog="EXTRA_DOMAINS=$EXTRA_DOMAINS ./_build/default/bench/$benchname/run.exe"
 
 dune build bench/$benchname/run.exe
 
