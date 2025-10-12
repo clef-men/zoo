0c17be87d34d478b53536a02ece39cfe60567001
Gabriel Scherer <gabriel.scherer@gmail.com>
2025-10-13 00:08:11 +0200

    bench/lu: refresh numbers for divide-and-conquer Pool.for

--- bench/lu/gen_plot_data_domains.sh
+++ bench/lu/gen_plot_data_domains.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
 benchname="lu"
 input="700"
-cutoff="1"
+cutoff="10"
 extra_domains="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14"
 impls="sequential parabs domainslib moonpool-fifo moonpool-ws"
 prog="CUTOFF=$cutoff ./_build/default/bench/$benchname/run.exe"
