8e6502ae23bd03b694e6bd31ebcb1b2339a42945
Clément <clef-men@orange.fr>
2026-02-22 22:37:38 +0100

    zoo/iris/base_logic/lib/base: Fix [solve_inG].

--- theories/zoo/iris/base_logic/lib/base.v
+++ theories/zoo/iris/base_logic/lib/base.v
@@ -10,3 +10,32 @@ From zoo Require Import
   prelude.
 From zoo Require Import
   options.
+
+Ltac solve_inG :=
+  intros;
+  lazymatch goal with
+  | H: subG (?xΣ _ _ _ _) _ |- _ =>
+      try unfold xΣ in H
+  | H: subG (?xΣ _ _ _) _ |- _ =>
+      try unfold xΣ in H
+  | H: subG (?xΣ _ _) _ |- _ =>
+      try unfold xΣ in H
+  | H: subG (?xΣ _) _ |- _ =>
+      try unfold xΣ in H
+  | H: subG ?xΣ _ |- _ =>
+      try unfold xΣ in H
+  end;
+  repeat match goal with
+  | H: subG (gFunctors.app _ _) _ |- _ =>
+      apply subG_inv in H; destruct H
+  end;
+  repeat match goal with
+  | H: subG _ _ |- _ =>
+      move: (H);
+      (apply subG_inG in H || clear H)
+  end;
+  intros;
+  simpl in *;
+  try assumption;
+  esplit;
+  (assumption || by apply _).
