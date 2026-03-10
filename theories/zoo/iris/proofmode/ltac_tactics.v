33c74c2fede228b9c72d635b5e0a04bb120b3565
Clément <clef-men@orange.fr>
2026-03-10 21:44:37 +0100

    Apply https://gitlab.mpi-sws.org/iris/iris/-/commit/25f29a6181d0713f52353d6a978aa02cdfbd031d.

--- theories/zoo/iris/proofmode/ltac_tactics.v
+++ theories/zoo/iris/proofmode/ltac_tactics.v
@@ -917,7 +917,7 @@ tactics below use [notypeclasses refine] instead of [apply], [split] and
        |lazymatch goal with (* Force [A] in [ex_intro] to deal with coercions. *)
         | |- ∃ _ : ?A, _ =>
           notypeclasses refine (@ex_intro A _ x _)
-        end; [shelve..|pm_reduce; iSpecializeArgs_go H xs]]
+        end; resolve_tc x; pm_reduce; iSpecializeArgs_go H xs]
   end.
 #[local] Tactic Notation "iSpecializeArgs" constr(H) open_constr(xs) :=
   iSpecializeArgs_go H xs.
