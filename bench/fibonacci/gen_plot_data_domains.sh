cb73d278b2c6f786b4d2385000e58de6277188de
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-03-24 17:42:01 +0100

    fibonacci: up-to-date results with both Moonpool and Taskflow

--- bench/fibonacci/gen_plot_data_domains.sh
+++ bench/fibonacci/gen_plot_data_domains.sh
@@ -6,7 +6,7 @@ benchname="fibonacci"
 input="42"
 cutoff="25"
 extra_domains="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14"
-impls="sequential parabs domainslib moonpool-ws"
+impls="sequential parabs domainslib moonpool-ws mooonpool-fifo"
 prog="CUTOFF=$cutoff ./_build/default/bench/$benchname/run.exe"
 
 dune build bench/$benchname/run.exe
