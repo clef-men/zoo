e2b5215fec7d775a1c5bd057b36393798fdde111
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-03-30 22:04:17 +0200

    Trigger: improve performance by creating the condvar on-demand

--- lib/zoo_parabs/trigger.ml
+++ lib/zoo_parabs/trigger.ml
@@ -1,31 +1,40 @@
 type t =
-  { mutex: Mutex.t
-  ; condition: Condition.t
+  { mutable cond : (Mutex.t * Condition.t) option [@atomic]
   ; predicate: unit -> bool
   }
 
 let create pred =
-  { mutex= Mutex.create ()
-  ; condition= Condition.create ()
+  { cond = None
   ; predicate= pred
   }
 
 let[@inline] probe t =
   t.predicate ()
 
-let notify_weak t =
-  Condition.notify t.condition ;
-  true
-
 let notify t =
-  Mutex.protect t.mutex @@ fun () ->
-    Condition.notify t.condition
+  match t.cond with
+  | None -> ()
+  | Some (mutex, condition) ->
+     Mutex.protect mutex @@ fun () ->
+     Condition.notify condition
+
+let notify_weak t =
+  notify t; true
+
+let rec init_cond t =
+  match t.cond with
+  | Some cond -> cond
+  | None ->
+    let cond = (Mutex.create (), Condition.create ()) in
+    if Atomic.Loc.compare_and_set [%atomic.loc t.cond] None (Some cond) then cond
+    else init_cond t
 
 let wait t =
-  Mutex.protect t.mutex @@ fun () ->
+  let (mutex, condition) = init_cond t in
+  Mutex.protect mutex @@ fun () ->
     if probe t then (
       true
     ) else (
-      Condition.wait t.condition t.mutex ;
+      Condition.wait condition mutex ;
       false
     )
