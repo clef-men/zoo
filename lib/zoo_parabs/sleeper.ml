d565db49a5b865f9dcfb8d17c3d6dbc694a9f847
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-03-31 21:58:05 +0200

    simplification: in Sleeper, remove 'sleeping'
    Suggested-by: Clément Allain <clef-men@orange.fr>

--- lib/zoo_parabs/sleeper.ml
+++ lib/zoo_parabs/sleeper.ml
@@ -2,13 +2,12 @@ type t =
   { id : int
   ; mutex : Mutex.t
   ; condition : Condition.t
-  ; mutable sleeping : bool
   ; mutable sleep_round : int
   ; mutable last_wakeup : int
   }
 (* [id] is useful to write debugging prints.
 
-   [sleeping], [sleep_round] and [last_wakeup] are protected by the mutex.
+   [sleep_round] and [last_wakeup] are protected by the mutex.
    [last_wakeup] is the round at which the last wakeup occurred:
    when [sleep_round = last_wakeup], the sleeper has been awoken
    after the last [prepare_sleep] call. *)
@@ -17,7 +16,6 @@ let create id =
   { id
   ; mutex = Mutex.create ()
   ; condition = Condition.create ()
-  ; sleeping = false
   ; sleep_round = 0
   ; last_wakeup = 0
   }
@@ -28,21 +26,18 @@ let wakeup t =
     (* we already received a wakeup *)
     false
   else (
-    (* even if we were not yet sleeping, the wakeup is 'useful' *)
     t.last_wakeup <- t.sleep_round;
-    if t.sleeping then Condition.notify t.condition;
+    Condition.notify t.condition;
     true
   )
 
 let prepare_sleep t =
   Mutex.protect t.mutex @@ fun () ->
-  assert (not t.sleeping);
   assert (t.last_wakeup <= t.sleep_round);
   t.sleep_round <- t.sleep_round + 1
 
 let cancel_sleep t =
   Mutex.protect t.mutex @@ fun () ->
-  assert (not t.sleeping);
   let no_wakeup_received = t.last_wakeup < t.sleep_round in
   (* pretend we receive a wakeup so that a future wakeup
      on this round is not counted as 'useful' *)
@@ -51,12 +46,9 @@ let cancel_sleep t =
 
 let commit_sleep t =
   Mutex.protect t.mutex @@ fun () ->
-  assert (not t.sleeping);
   if t.sleep_round = t.last_wakeup then (
     (* we received a notification between [prepare] and [commit] *)
     ()
   ) else (
-    t.sleeping <- true;
     Condition.wait t.condition t.mutex;
-    t.sleeping <- false
   )
