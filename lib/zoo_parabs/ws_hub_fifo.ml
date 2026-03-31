5a61af58eb2acddc18acb432bd98b5c241674811
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-04-01 00:44:59 +0200

    adapt ws_hub_fifo (untested)

--- lib/zoo_parabs/ws_hub_fifo.ml
+++ lib/zoo_parabs/ws_hub_fifo.ml
@@ -1,14 +1,16 @@
 type 'a t =
   { size: int
   ; queue: 'a Mpmc_queue_1.t
-  ; waiters: Waiters.t
+  ; sleepers: Sleeper.t array
+  ; dormitory: Dormitory.t
   ; mutable killed: bool
   }
 
 let create sz =
   { size= sz
   ; queue= Mpmc_queue_1.create ()
-  ; waiters= Waiters.create ()
+  ; sleepers= Array.unsafe_initi sz Sleeper.create
+  ; dormitory= Dormitory.create ()
   ; killed= false
   }
 
@@ -24,78 +26,57 @@ let unblock _t _i =
 let killed t =
   t.killed
 
-let notify t =
-  Waiters.notify t.waiters (size t)
-let notify_all t =
-  Waiters.notify_all t.waiters
-
 let push t _i v =
   Mpmc_queue_1.push t.queue v ;
-  notify t
+  Dormitory.wakeup_one t.dormitory
 
 let pop' t =
   Mpmc_queue_1.pop t.queue
 let pop t _i =
   pop' t
 
-let rec steal_until_aux t max_round trigger =
-  match pop' t with
-  | Some _ as res ->
-      res
-  | None ->
-      if Trigger.probe trigger then
-        None
-      else (
-        Domain.yield () ;
-        steal_until_aux t (max_round - 1) trigger
-      )
-let rec steal_until t max_round trigger =
-  match steal_until_aux t max_round trigger with
-  | Some _ as res ->
-      res
-  | None ->
-      Waiters.push t.waiters trigger ;
-      if Trigger.wait trigger then
-        None
-      else
-        steal_until t max_round trigger
-let steal_until t _i _max_round_noyield max_round_yield trigger =
-  steal_until t max_round_yield trigger
-
-let rec steal t =
-  let waiters = t.waiters in
-  let trigger = Waiters.prepare_wait waiters in
-  if killed t then (
-    Waiters.cancel_wait waiters trigger (size t) ;
+let rec steal_aux t i ~finished ~prepare_sleep =
+  let sleeper = t.sleepers.(i) in
+  Sleeper.prepare_sleep sleeper;
+  Dormitory.push t.dormitory sleeper;
+  prepare_sleep (fun () -> ignore (Sleeper.wakeup sleeper));
+  if finished () then (
+    if not (Sleeper.cancel_sleep sleeper) then
+      Dormitory.wakeup_one t.dormitory;
     None
   ) else (
-    if Mpmc_queue_1.is_empty t.queue then (
-      Waiters.commit_wait waiters trigger
-    ) else (
-      Waiters.cancel_wait waiters trigger (size t)
-    ) ;
-    match pop' t with
+    match pop t i with
     | Some _ as res ->
+        (* We are stealing a task that woke someone up,
+           so they will have a spurious wakeup. *)
+        ignore (Sleeper.cancel_sleep sleeper);
         res
     | None ->
-        steal t
+        Sleeper.commit_sleep sleeper;
+        steal_aux t i ~finished ~prepare_sleep
   )
-let steal t _i _max_round_noyield _max_round_yield =
-  steal t
+
+let steal_until t i _max_round_noyield _max_round_yield ~finished ~prepare_sleep =
+  steal_aux t i ~finished ~prepare_sleep
+
+let steal t i _max_round_noyield _max_round_yield =
+  steal_aux t i
+    ~finished:(fun () -> killed t)
+    ~prepare_sleep:ignore
 
 let kill t =
   t.killed <- true ;
-  notify_all t
+  Dormitory.wakeup_all t.dormitory
 
-let pop_steal_until t i max_round_noyield max_round_yield trigger =
-  if Trigger.probe trigger then
+let pop_steal_until t i max_round_noyield max_round_yield ~finished ~prepare_sleep =
+  if finished () then
     None
   else
     match pop t i with
     | Some _ as res ->
         res
     | None ->
-        steal_until t i max_round_noyield max_round_yield trigger
+        steal_until t i max_round_noyield max_round_yield ~finished ~prepare_sleep
 
 let pop_steal t i max_round_noyield max_round_yield =
   match pop t i with
