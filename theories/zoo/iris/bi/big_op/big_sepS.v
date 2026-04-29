cf754b26abf65244ddea457eefec0a1bd3d2d6fe
Clément <clef-men@orange.fr>
2026-04-29 12:53:09 +0200

    zoo/iris/bi/big_op/big_sepS: Add [big_sepS_exists].

--- theories/zoo/iris/bi/big_op/big_sepS.v
+++ theories/zoo/iris/bi/big_op/big_sepS.v
@@ -57,5 +57,23 @@ Section bi.
       iApply (big_sepL_impl_thread_fupd with "Hs HP"). iIntros "!>" (k x Hx%list_elem_of_lookup_2%elem_of_elements) "HΦ1 HP".
       iApply ("HΦ" with "[//] HΦ1 HP").
     Qed.
+
+    Lemma big_sepS_exists `{!BiAffine PROP} {V} (Φ : K → V → PROP) s :
+      ([∗ set] x ∈ s, ∃ v, Φ x v) ⊢
+        ∃ m,
+        ⌜dom m = s⌝ ∗
+        [∗ map] x ↦ v ∈ m, Φ x v.
+    Proof.
+      rewrite big_sepS_elements big_sepL_exists.
+      setoid_rewrite big_sepL2_alt.
+      iIntros "(%vs & %Hvs & _ & H)".
+      iDestruct (big_sepM_list_to_map with "H") as "$".
+      { rewrite fst_zip. 1: lia.
+        apply NoDup_elements.
+      }
+      iPureIntro.
+      rewrite dom_list_to_map_L fst_zip. 1: lia.
+      rewrite list_to_set_elements_L //.
+    Qed.
   End big_sepS.
 End bi.
