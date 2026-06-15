d6d346ab3d9e0bd0199205b9e0d4a58ec576d863
Clément <clef-men@orange.fr>
2026-06-15 17:41:32 +0200

    zoo_saturn: Add svar.

---
+++ theories/zoo_saturn/svar__opaque.v
@@ -0,0 +1,9 @@
+From zoo_saturn Require Import
+  svar__code.
+
+#[global] Opaque svar٠make.
+#[global] Opaque svar٠forward.
+#[global] Opaque svar٠get.
+#[global] Opaque svar٠set.
+#[global] Opaque svar٠click.
+#[global] Opaque svar٠observe.
